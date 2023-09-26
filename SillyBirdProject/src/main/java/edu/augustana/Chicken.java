3d48b680ba07188e9bc98b4a02fd56cf8865858b
Aidan <aidanhancock22@augustana.edu>
2023-09-26 13:55:57 -0500

    "Now with dancing chicken"

---
+++ SillyBirdProject/src/main/java/edu/augustana/Chicken.java
@@ -0,0 +1,12 @@
+package edu.augustana;
+
+public class Chicken {
+
+    public Chicken(){
+
+    }
+    public static void dance(){
+        System.out.println("Chicken dance!");
+    }
+
+}
--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -1,7 +1,11 @@
 package edu.augustana;
 
-public class Main {
+public class Main extends Chicken{
+    static Chicken chicken = new Chicken();
+
     public static void main(String[] args) {
+        chicken.dance();
         System.out.println("Hello bird world!");
     }
+
 }
